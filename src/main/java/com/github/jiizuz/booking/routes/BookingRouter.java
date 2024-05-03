8dd4dce83f8df0189cde3688773643e26a6b9a32
Jiizuz <masterchack92@hotmail.com>
2024-05-03 16:04:39 -0600

    feat: router added to map the bookings handlers

---
+++ src/main/java/com/github/jiizuz/booking/routes/BookingRouter.java
@@ -0,0 +1,22 @@
+package com.github.jiizuz.booking.routes;
+
+import io.muserver.MuServerBuilder;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+/**
+ * Router that sets up the routes of the bookings.
+ *
+ * @author <a href="mailto:masterchack92@hotmail.com">Jiizuz</a>
+ * @since 1.0
+ */
+@NoArgsConstructor
+public final class BookingRouter implements Router {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setupRoutes(final @NonNull MuServerBuilder builder) {
+    }
+}
