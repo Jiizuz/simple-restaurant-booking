94a4892c50b743e43841d2c4d36e999c2e4b1267
Jiizuz <masterchack92@hotmail.com>
2024-05-03 16:31:55 -0600

    feat: functionality added to the booking to generate immutable copies and #withId() function

--- src/main/java/com/github/jiizuz/booking/booking/ImmutableBooking.java
+++ src/main/java/com/github/jiizuz/booking/booking/ImmutableBooking.java
@@ -19,6 +19,7 @@ public final class ImmutableBooking implements Booking {
     /**
      * The UniqueId of the booking. Can be {@code null} if the booking is not persisted.
      */
+    @With
     @Builder.Default
     private final String id = null;
 
@@ -38,4 +39,20 @@ public final class ImmutableBooking implements Booking {
      */
     @NonNull
     private final Instant date;
+
+    /**
+     * Creates a {@link ImmutableBooking} that copies the data of the given {@link Booking}.
+     *
+     * @param booking the booking to copy
+     * @return the immutable booking with the data of the given booking
+     * @throws NullPointerException if the booking is {@code null}
+     */
+    public static ImmutableBooking copyOf(final @NonNull Booking booking) {
+        return ImmutableBooking.builder()
+                .id(booking.getId())
+                .customerName(booking.getCustomerName())
+                .tableSize(booking.getTableSize())
+                .date(booking.getDate())
+                .build();
+    }
 }
